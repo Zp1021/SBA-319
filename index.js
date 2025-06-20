b77c3f6d20c2a2245c45c004a35280a37a621b65
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2025-06-20 12:09:48 -0400

    Successfully created a running express server

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+import 'dotenv/config'
+
+
+const app = express();
+
+const PORT = process.env.PORT
+
+app.get ('/', (req, res) => {
+    res.send(`<h1>Hello World!
+        <br>Welcome to SBA 319!</h1>`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
